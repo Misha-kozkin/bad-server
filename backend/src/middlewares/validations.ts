4073cb21b5d7e4e35061efafcb0034544aab3a0e
Mikhail Kozkin <Mikhail.Kozkin@yandex.kz>
2026-07-22 18:17:48 -0700

    fix(security): replace vulnerable phone regex to prevent ReDoS

--- backend/src/middlewares/validations.ts
+++ backend/src/middlewares/validations.ts
@@ -2,7 +2,8 @@ import { Joi, celebrate } from 'celebrate'
 import { Types } from 'mongoose'
 
 // eslint-disable-next-line no-useless-escape
-export const phoneRegExp = /^(\+\d+)?(?:\s|-?|\(?\d+\)?)+$/
+// первый вориант /^\+?\d{1,4}?[\s()-]*\d{1,4}[\s()-]*\d{1,4}[\s()-]*\d{1,4}[\s()-]*\d{0,4}$/
+export const phoneRegExp = /^\+?[\d\s\-()]{5,20}$/ 
 
 export enum PaymentType {
     Card = 'card',

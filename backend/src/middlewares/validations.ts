3974423e5e135d2e2d6c98c1ade51c83205644c3
Mikhail Kozkin <Mikhail.Kozkin@yandex.kz>
2026-07-22 18:53:05 -0700

    fix(security): escape order comments and limit input length

--- backend/src/middlewares/validations.ts
+++ backend/src/middlewares/validations.ts
@@ -45,7 +45,7 @@ export const validateOrderBody = celebrate({
         total: Joi.number().required().messages({
             'string.empty': 'Не указана сумма заказа',
         }),
-        comment: Joi.string().optional().allow(''),
+        comment: Joi.string().optional().allow('').max(300),
     }),
 })
 
